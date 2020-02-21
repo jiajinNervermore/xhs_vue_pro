61f56bbc773b447833f153735e471350afde8641
jiajinNervermore <58249513+jiajinNervermore@users.noreply.github.com>
2020-02-21 09:06:10 +0800

    0221

---
+++ src/components/xhs/shopping_cart.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="shopping_cart">
+    <div class="header"></div>
+    <div class="activity"></div>
+    <!-- 应用子组件shopping_cart_shop 添加商品栏 -->
+    <shop :list="list"></shop>
+  </div>
+</template>
+<script>
+//引入子组件shop
+import shop from "./shopping_cart/shopping_cart_shop.vue";
+export default {
+  components: { shop },
+  data() {
+    return {
+      list: [
+        {
+          store: "兰蔻",
+          count: 1,
+          detail: "保湿水",
+          capacity: "150ml",
+          inventory: 10,
+          price: 259,
+          is_activity: true,
+          special_offer: "满300减30元",
+          pic:"e61865991d0c4e19.jpg",
+          is_checked:false
+        },
+        {
+          store: "谷雨",
+          count: 1,
+          detail: "面膜",
+          capacity: "150ml",
+          inventory: 10,
+          price: 409,
+          is_activity: true,
+          special_offer: "满300减30元",
+          pic:"e61865991d0c4e19.jpg",
+          is_checked:false
+        }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 import Vue from 'vue' //引入Vue对象
 import Router from 'vue-router'//引入路由
-//引入自定义组件
+//引入自定义组件 注册
 import register from './components/xhs/register.vue'
+//引入自定义组件 购物车
+import shopping from './components/xhs/shopping_cart'
 Vue.use(Router)//将路由对象注册
 //配置组件访问路径
 export default new Router({
   routes:[
-    {path:"/signin",component:register}
+    {path:"/signin",component:register},
+    {path:"/shopping",component:shopping}
   ]
 })
