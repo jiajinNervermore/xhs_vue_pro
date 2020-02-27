aac44a8117476072b73309a742554de73ee992bb
jiangfujie <2239349799@qq.com>
2020-02-27 20:49:38 +0800

    3.0

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import Vue from 'vue' //引入Vue对象
 import Router from 'vue-router'//引入路由
 //引入自定义组件
 import register from './components/xhs/register.vue'
+import Carousel from './views/carousel'
 Vue.use(Router)//将路由对象注册
 //配置组件访问路径
 export default new Router({
   routes:[
-    {path:"/signin",component:register}
+    {path:"/signin",component:register},
+    {path:"/carousel",component:Carousel}
   ]
 })
---
+++ src/views/recommend.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="con">
+    <ul>
+      <li @click="changeActive(i)" :class="{active:l.isActive}" v-for="(l,i) of list" :key="i">{{l.title}}</li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+  .con{
+    padding: 8px;
+  }
+  ul{
+    position: relative;
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+  }
+  ul>li{
+    text-align: center;
+    display: block;
+    color: #999;
+    font-size: 18px;
+    width: 16.666%;
+    padding: 4px;
+  }
+  .active{
+    color: black;
+    font-weight:bolder;
+    position: relative;
+    border-bottom: 3px solid red;
+  }
+</style>
+<script>
+export default {
+  data(){
+    return {
+      list:[
+        {title:"推荐",isActive:true},
+        {title:"女神",isActive:false},
+        {title:"护肤",isActive:false},
+        {title:"彩妆",isActive:false},
+        {title:"个护",isActive:false},
+        {title:"保健",isActive:false},
+      ],
+    }
+  },
+  methods:{
+    changeActive(i){
+      for(var item of this.list){
+        item.isActive=false;
+      }
+      this.list[i].isActive=true;
+    }
+  }
+}
+</script>
