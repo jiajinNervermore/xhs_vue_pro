122d1d6d787f2e59ee2bfb6c2fc9717376c6b59f
jiang <22393497992@qq.com>
2020-03-12 10:50:55 +0800

    3.12

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ import Router from 'vue-router'//引入路由
 //引入自定义组件
 import register from './components/xhs/register.vue'
 import Carousel from './views/carousel'
+import Indexcq from './views/indexcq'
 Vue.use(Router)//将路由对象注册
 //配置组件访问路径
 export default new Router({
   routes:[
+    {path:"/indexcq",component:Indexcq},
     {path:"/signin",component:register},
     {path:"/carousel",component:Carousel}
   ]
